6bf60b12ed1ce487e6c144f0c6c7aa839eed1f7c
Muadeel56 <muadeel59@gmail.com>
2024-09-13 00:23:19 +0500

    Creating New Project

---
+++ instagram-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <div className='w-full min-h-screen bg-black'>
+        <h1 className='text-white'>This is main section for now</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
